8b70f1dda15aa037e4bc5d66b5582590defef2fd
Vasiliy Golubev <vas@adriver.ru>
2020-11-14 17:00:50 +0300

    added checks for hot water temperature and shp-13

--- lua/init.lua
+++ lua/init.lua
@@ -1,3 +1,5 @@
 telegram.settoken("TO:KEN")
 telegram.setchat("CHAT_ID")
+zigbee.setsaveinterval(0)
+obj.set("hot_water_over", "1")
 
--- lua/onemintimer.lua
+++ lua/onemintimer.lua
@@ -21,3 +21,45 @@ for device, devicename in pairs(doors) do
   end
 end
 
+--
+-- Проверка температуры горячей воды
+--
+local gmt = 3
+local time = os.time()
+local hour = (math.modf(time / 3600) + gmt) % 24
+if hour >= 22 or hour <6 then
+  threshhold = 7200
+else
+  threshhold = 3600
+end
+
+if (zigbee.value("0x00158D000533D108", "temperature")) <= 30 then
+  if not obj.get("hot_water_over") or obj.get("hot_water_over") == "1" then
+    obj.set("hot_water_over", os.time())
+  end
+  if obj.get("hot_water_alert_last_sent") and (os.time()-obj.get("hot_water_alert_last_sent") >= threshhold) then
+    telegram.send ("\xef\xbf\xbc\xf0\x9f\x9a\xb1 Похоже, отключили горячую воду. Температура трубы: ".. string.format("%.2f", zigbee.value("0x00158D000533D108", "temperature")) .. " °C")
+    obj.set("hot_water_alert_last_sent", os.time())
+  elseif not obj.get("hot_water_alert_last_sent") then
+    telegram.send ("\xef\xbf\xbc\xf0\x9f\x9a\xb1 Похоже, отключили горячую воду. Температура трубы: ".. string.format("%.2f", zigbee.value("0x00158D000533D108", "temperature")) .. " °C")
+    obj.set("hot_water_alert_last_sent", os.time())
+  end
+else
+  if obj.get("hot_water_over") ~= "1" then
+    telegram.send ("\xef\xbf\xbc\xf0\x9f\x9a\xb0 УРА! Дали горячую воду. Температура трубы: " .. string.format("%.2f", zigbee.value("0x00158D000533D108", "temperature")) .. " °C. Воды не было " .. string.format("%.2f", (os.time()-obj.get("hot_water_over"))/3600) .. " час.")
+    obj.set("hot_water_over", "1")
+    obj.set("hot_water_alert_last_sent", "1")
+  end
+end
+
+--
+-- Опрос розеток SHP-13
+--
+shp13_sockets = {["0x842E14FFFE1394F4"]="Чайник", ["0x842E14FFFE139336"]="Бойлер", ["0x842E14FFFE139485"]="Стиральная машина"}
+for device, devicename in pairs(shp13_sockets) do
+--  if zigbee.value(device, "state") == "ON" then
+    zigbee.get(device, "power")
+    zigbee.get(device, "current")
+--  end
+end
+
